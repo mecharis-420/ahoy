3fb006087379e744c41e80ec632c694e820c319c
Xtra Studios US <148826402+xtrastudioUS@users.noreply.github.com>
2024-02-11 13:25:18 -0800

    Create parsing.js
                     >>> PROBABLY VERY BUGGY! <<<
     This is an attempt to start off the source of the parsing script

---
+++ seas/parsing.js
@@ -0,0 +1,10 @@
+// Parses the JSON file and looks for keywords, printing out the "desc" and "name" tags
+
+var tags = [
+ const tag_name = '"name"';
+ const tag_description = '"desc"';
+ const file = "ahoyfile";
+];
+
+
+SHOW = JSON.parse(tags);
