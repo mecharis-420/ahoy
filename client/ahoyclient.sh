2ec7218a77224c5c091011aa1276b6f7d684b5f9
Xtra Studios US <148826402+xtrastudiosUS@users.noreply.github.com>
2024-02-11 14:16:38 -0800

    made the Linux (Bash) client

---
+++ client/ahoyclient.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+alias sail="./ahoyclient"
+
+version="1.00"
+# option: -d  /[DIRECTORY REQUESTED]/
+sudo systemctl start httpd
+sudo systemctl status httpd
+
+# option: -help
+Help(){
+echo "Ahoy! version:$version"
+echo "options:"
+
+
+
+
+
+
+
+
+
+}
+
+
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+   esac
+done
